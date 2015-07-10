e3aa32117b97ee76cdb77e0c51b06eb9a9fb1499
Stephanie <stephmpi@gmail.com>
2015-07-10 14:39:20 -0700

    init

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+
+  // set the size of the canvas inside the window.
+  createCanvas(1000, 600);
+
+  // set the background color
+  background(255);
+
+}
+
+
+// this is run repeatedly.
+function draw() {
+
+}
